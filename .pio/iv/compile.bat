e4c9e956815600378f1c6334c2fd6ec0cc9e177c
Daniel Robinson <d7robin@gmail.com>
2019-07-11 01:02:16 +0200

    !v

--- .pio/iv/compile.bat
+++
@@ -1,4 +0,0 @@
-pandoc invoice.md -F pandoc-crossref -F pandoc-citeproc --mathjax --standalone --number-sections -H fix-captions.tex -o invoice.pdf
-rem "timeout /t 10"
-rem "pause"
-rem "-F mermaid-filter.cmd"
